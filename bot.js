5618b3b9f15f8ab20ae03f7cdaf7878952d9e325
kelinhakcs5-beep <kelinhakcs5@gmail.com>
2026-07-13 21:39:46 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,13 @@ const WebSocket = require("ws");
 
 const WS_URL = "wss://live-api.casinoscores.com/ws";
 
+let ws;
+let heartbeat;
+
+function conectar() {
 console.log("Iniciando conexao com o Red Door...");
 
-const ws = new WebSocket(WS_URL, "v12.stomp", {
+ws = new WebSocket(WS_URL, "v12.stomp", {
 headers: {
 Origin: "https://www.casino.org"
 }
@@ -23,10 +27,45 @@ ws.send(
 ws.on("message", (data) => {
 const mensagem = data.toString();
 
+if (mensagem.startsWith("CONNECTED")) {
+console.log("STOMP conectado!");
+
+heartbeat = setInterval(() => {
+if (ws.readyState === WebSocket.OPEN) {
+ws.send("\n");
+}
+}, 10000);
+
+ws.send(
+"SUBSCRIBE\n" +
+"id:sub-0\n" +
+"destination:/topic/spins/red-door-roulette\n\n\0"
+);
+
+console.log("Canal dos giros assinado!");
+return;
+}
+
+if (mensagem.trim() !== "") {
 console.log("Mensagem recebida:", mensagem);
+}
 });
 
 ws.on("error", (erro) => {
 console.error("Erro no WebSocket:", erro.message);
 });
+
+ws.on("close", () => {
+console.log("WebSocket desconectado.");
+
+clearInterval(heartbeat);
+
+setTimeout(() => {
+console.log("Tentando reconectar...");
+conectar();
+}, 5000);
+});
+}
+
+conectar();
   
